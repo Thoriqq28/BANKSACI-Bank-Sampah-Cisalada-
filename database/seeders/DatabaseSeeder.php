8a0db3e449184708ff578c9d57ca51cfc6f6e442
Thoriqq28 <thoriqra968@gmail.com>
2026-08-01 17:06:57 +0700

    feat: complete master data for jenis_sampah in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -48,7 +48,7 @@ class DatabaseSeeder extends Seeder
             ]
         );
 
-        // 4. Seed Kategori Sampah (Diubah: nama_kategori -> nama)
+        // 4. Seed Kategori Sampah
         DB::table('kategori_sampah')->updateOrInsert(
             ['id' => 1],
             [
@@ -58,34 +58,26 @@ class DatabaseSeeder extends Seeder
             ]
         );
 
-        // 5. Seed Jenis Sampah
+        // 5. Seed Lengkap Jenis Sampah
         $jenisSampahData = [
-            [
-                'id' => 1,
-                'kategori_id' => 1,
-                'nama' => 'Kertas / Kardus',
-                'harga_beli' => 1500,
-                'harga_per_kg' => 2000,
-                'satuan' => 'Kg',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'id' => 7, // Disesuaikan agar transaksi ID 7 tidak lagi error
-                'kategori_id' => 1,
-                'nama' => 'Botol Plastik',
-                'harga_beli' => 2500,
-                'harga_per_kg' => 3200,
-                'satuan' => 'Kg',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
+            ['id' => 1, 'kategori_id' => 1, 'nama' => 'Besi', 'harga_beli' => 3500, 'harga_per_kg' => 4500, 'satuan' => 'Kg'],
+            ['id' => 2, 'kategori_id' => 1, 'nama' => 'Kaleng', 'harga_beli' => 1500, 'harga_per_kg' => 2000, 'satuan' => 'Kg'],
+            ['id' => 3, 'kategori_id' => 1, 'nama' => 'Buku', 'harga_beli' => 800, 'harga_per_kg' => 1200, 'satuan' => 'Kg'],
+            ['id' => 4, 'kategori_id' => 1, 'nama' => 'Kardus', 'harga_beli' => 1200, 'harga_per_kg' => 1800, 'satuan' => 'Kg'],
+            ['id' => 5, 'kategori_id' => 1, 'nama' => 'Kuningan', 'harga_beli' => 80000, 'harga_per_kg' => 100000, 'satuan' => 'Kg'],
+            ['id' => 6, 'kategori_id' => 1, 'nama' => 'Tembaga', 'harga_beli' => 150000, 'harga_per_kg' => 180000, 'satuan' => 'Kg'],
+            ['id' => 7, 'kategori_id' => 1, 'nama' => 'Aqua Gelas dan Semacamnya', 'harga_beli' => 2200, 'harga_per_kg' => 3000, 'satuan' => 'Kg'],
+            ['id' => 8, 'kategori_id' => 1, 'nama' => 'Botol Plastik', 'harga_beli' => 2500, 'harga_per_kg' => 3200, 'satuan' => 'Kg'],
+            ['id' => 9, 'kategori_id' => 1, 'nama' => 'Ember', 'harga_beli' => 1800, 'harga_per_kg' => 2500, 'satuan' => 'Kg'],
         ];
 
         foreach ($jenisSampahData as $data) {
             DB::table('jenis_sampah')->updateOrInsert(
                 ['id' => $data['id']],
-                $data
+                array_merge($data, [
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ])
             );
         }
     }
